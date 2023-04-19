2a17ea3b0f860f5bffa8edb5f50f33825214afdc
arnab <arnabchatterjee.ac.2@gmail.com>
2023-04-19 23:41:43 +0530

    added copying color

--- src/components/ColorViewer.jsx
+++ src/components/ColorViewer.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function ColorViewer({color}) {
+export default function ColorViewer({ color }) {
+  const initialText = "copy";
+  const [defaultText, setDefaultText] = useState(initialText);
+  const [display, setDisplay] = useState(false);
+  const clickHandler = async() => {
+    setDefaultText("copied");
+    await navigator.clipboard.writeText(color);
+  };
+  const leaveHandler = () => {
+    setDefaultText(initialText);
+    setDisplay(false);
+  };
   return (
-    <div className='w-20 h-20 rounded border-2' style={{backgroundColor:color}}/>
-  )
+    <div
+      className="w-20 h-20 rounded border-2 relative cursor-pointer"
+      style={{ backgroundColor: color }}
+      onMouseEnter={() => setDisplay(true)}
+      onMouseLeave={leaveHandler}
+    >
+      {display && (
+        <div
+          onClick={clickHandler}
+          className="w-full h-full rounded flex justify-center items-center bg-slate-50 absolute"
+        >
+          <p className="text-black">{defaultText}</p>
+        </div>
+      )}
+    </div>
+  );
 }
