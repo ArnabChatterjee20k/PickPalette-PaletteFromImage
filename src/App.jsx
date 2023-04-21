2087fbdda319c1eef981b4aeb54c0ec33c74d425
arnab <arnabchatterjee.ac.2@gmail.com>
2023-04-21 22:20:07 +0530

    Route -- Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ImageUpload from "./Page/ImageUpload/ImageUpload";
+// import ImageUpload from "./Page/ImageUpload/ImageUpload";
+// import Home from "./Page/Home/assets/Home";
+
+const Home = lazy(() => import("./Page/ImageUpload/ImageUpload"));
+const ImageUpload = lazy(() => import("./Page/ImageUpload/ImageUpload"));
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <Container>
         <Navbar />
-        <ImageUpload/>
+        <BrowserRouter>
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/generate" element={<ImageUpload />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
         <Footer />
       </Container>
     </>
