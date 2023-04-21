ffc691348eaed91031e83bdda971463e088de871
arnab <arnabchatterjee.ac.2@gmail.com>
2023-04-22 00:25:32 +0530

    Feat -- Added redirection to generate page

--- src/Page/Home/components/RedirectButton.jsx
+++ src/Page/Home/components/RedirectButton.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 export default function RedirectButton({className}) {
     return (
-      <a
-        href="#"
+      <Link
+        to="/generate"
         className={`inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-black rounded-lg bg-[#F7C04A] hover:bg-[#ffae00] focus:ring-4 focus:ring-yellow-500 ${className}`}
       >
         Generate
-      </a>
+      </Link>
     );
   }
