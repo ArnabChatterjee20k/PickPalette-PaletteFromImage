25c713be965fc3f76bfc0dd19ebcb8ce946cb24e
arnab <arnabchatterjee.ac.2@gmail.com>
2023-05-04 00:04:16 +0530

    Added newsletter option

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -6,7 +6,7 @@ import navLinks from "../data/navLinks";
 export default function MobileNav() {
   const { pathname } = useLocation();
   return (
-    <div className="shadow-md w-32 flex flex-col z-30 absolute right-6 top-[1.4rem]">
+    <div className="shadow-md w-36 flex flex-col z-30 absolute right-6 top-[1.4rem]">
       <Menu>
         <Menu.Button className="self-end focus:ring-gray-600 bg-gray-800 border-gray-700 text-white hover:bg-gray-700 mr-2 mb-2 font-medium rounded-lg text-sm px-3 py-1.5 text-center inline-flex items-center ">
           <HamburgerMenu className="w-6 h-6" />
@@ -22,12 +22,19 @@ export default function MobileNav() {
               </Item>
             );
           })}
+          <Item className="border-t font-extrabold border-t-gray-500 text-transparent bg-clip-text bg-gradient-to-r from-[#EEAB57] to-[#F7C04A]">
+            <Link>
+              NewsLetter
+            </Link>
+          </Item>
         </Menu.Items>
       </Menu>
     </div>
   );
 }
 
-const Item = ({ children }) => {
-  return <Menu.Item className="block px-4 py-2">{children}</Menu.Item>;
+const Item = ({ children, className}) => {
+  return (
+    <Menu.Item className={`block px-4 py-2 ${className}`}>{children}</Menu.Item>
+  );
 };
