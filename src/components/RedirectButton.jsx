49820697a770d28e15fe17e2facf703c3827de9a
arnab <arnabchatterjee.ac.2@gmail.com>
2023-05-07 18:35:08 +0530

    Added color to the redirect buttons

--- src/components/RedirectButton.jsx
+++ src/components/RedirectButton.jsx
@@ -3,7 +3,7 @@ export default function RedirectButton({className,buttonTitle,link}) {
     return (
       <Link
         to={link}
-        className={`inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-black rounded-md  ${className}`}
+        className={`inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center rounded-md  ${className}`}
       >
         {buttonTitle}
       </Link>
