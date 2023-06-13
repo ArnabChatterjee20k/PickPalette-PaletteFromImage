2d35808ba7500bb1ef708101a2cf24adab65ac04
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-13 19:33:57 +0530

    Style Fixed -- Comment Card. Adjusted the width

--- src/Page/Feedback/components/CommentCard.jsx
+++ src/Page/Feedback/components/CommentCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CommentCard = ({comment,author}) => {
   return (
-    <div className="p-6 border rounded-lg  bg-gray-800 border-gray-700 hover:bg-gray-700 shadow-lg">
+    <div className="p-6 border rounded-lg  bg-gray-800 border-gray-700 hover:bg-gray-700 shadow-lg min-w-fit">
       <p className="text-white">{comment}</p>
       <p className="text-gray-500">{author}</p>
     </div>
