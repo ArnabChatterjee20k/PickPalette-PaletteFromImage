69c6a68d97c7294d3e232206222c38f2c7c451f6
arnab <arnabchatterjee.ac.2@gmail.com>
2023-12-28 15:50:50 +0530

    feat -- updated the auth redirect url

--- src/Page/Feedback/components/HeroText.jsx
+++ src/Page/Feedback/components/HeroText.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 export default function HeroText() {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col items-center gap-4 mx-auto py-6">
-=======
     <div className="flex flex-col items-center gap-4 mx-auto py-6 text-center">
->>>>>>> feat/feedback
       <h2 className="text-4xl text-white font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
         Join the Conversation
       </h2>
