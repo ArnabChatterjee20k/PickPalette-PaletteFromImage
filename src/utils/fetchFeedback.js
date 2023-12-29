d5ede65bd10df2b6ebe3ed1c6014913211228274
arnab <arnabchatterjee.ac.2@gmail.com>
2023-12-29 15:20:21 +0530

    feat -- added feedback ordering

--- src/utils/fetchFeedback.js
+++ src/utils/fetchFeedback.js
@@ -2,6 +2,6 @@ import { VITE_MAIN_API } from "../data/data";
 import supabaseClient from "../supabaseClient";
 
 export default async()=>{
-    const {data,statusText} = await supabaseClient.from("feedback").select("feedback").order("sentiment_score",{ascending:"false"})
+    const {data,statusText} = await supabaseClient.from("feedback").select("feedback").order("sentiment_score",{ascending:false})
     return data
 }
