7a7938f51e3df7ba84807185d64b51fadacbfdb8
arnab <arnabchatterjee.ac.2@gmail.com>
2023-12-29 15:40:10 +0530

    upadated link

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 export const APP_NAME = "PickPalette"
 export const FOOTER_LINK = "https://www.linkedin.com/in/arnab-chatterjee-905ab3219/"
-export const ABOUT_LINK = "https://github.com/ArnabChatterjee20k/PickPalette-PaletteFromImage"
+export const ABOUT_LINK = "https://github.com/ArnabChatterjee20k"
 export const PALETTE_API = import.meta.env.VITE_PALETTE_API
 export const API_MAX_PAGE = import.meta.env.VITE_API_MAX_PAGE
 
