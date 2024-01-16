569f388ea68d18d3e7bee81d1a66c21c88e55db0
arnab <arnabchatterjee.ac.2@gmail.com>
2024-01-16 16:02:55 +0530

    feat -- updated the store actions of palletStore

--- src/Page/ProjectPalette/store/paletteStore.jsx
+++ src/Page/ProjectPalette/store/paletteStore.jsx
@@ -1,26 +1,24 @@
 import { create } from "zustand";
 
 export const useActionStore = create((set) => ({
-  colorStack: [],
+  colorStack: [], // for now it will be containing one set of colors only but we can use it to gather colors in LIFO or FIFO manner.
   isScheduled: false,
 }));
 
 export const addToStack = (color) =>
   useActionStore.setState((state) => ({
-    colorStack: [...state.colorStack, color], // Fix: Update state.colorStack
+    colorStack:  color // Fix: Update state.colorStack
   }));
 
 export const noMoreScheduling = () =>
-  useActionStore.setState({ isScheduled: false });
+  useActionStore.setState({ isScheduled: false,colorStack:[] });
 
 export const startScheduling = () =>
   useActionStore.setState({ isScheduled: true });
 
 export const isScheduled = ()=> useActionStore.getState().isScheduled
 
-export const emptySchedule = () => useActionStore.setState({ colorStack: [] });
-
 export const getLatestColorChanged = () => {
   const stack = useActionStore.getState().colorStack;
-  return stack[stack.length - 1];
+  return stack;
 };
