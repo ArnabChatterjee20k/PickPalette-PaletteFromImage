9e6cea09c4f62c4c012b3d99f5b44de94f0cfca2
arnab <arnabchatterjee.ac.2@gmail.com>
2024-01-28 21:37:14 +0530

    bug -- added env variables

---
+++ src/Page/ProjectPalette/actions/paletteActions.jsx
@@ -0,0 +1,65 @@
+import { useMutation, useQueryClient, useMutationState } from "@tanstack/react-query";
+import changeProjectPalette from "../utils/changeProjectPalette";
+import { useParams } from "react-router-dom";
+import {
+  getLatestColorChanged,
+  addToStack,
+  isScheduled,
+  startScheduling,
+  noMoreScheduling,
+} from "../store/paletteStore";
+
+export default function paletteActions() {
+  const client = useQueryClient();
+  const { id } = useParams();
+  const queryKey = ["project-palette", id];
+  const mutationKey = ["project-palette-update", id];
+  const savingState = useMutationState({filters:{mutationKey:mutationKey,status:"pending"}})
+  const { mutate } = useMutation({
+    mutationKey: mutationKey,
+    mutationFn: (data) => changeProjectPalette(data),
+  });
+
+  function changePalette(color, idx) {
+    client.setQueryData(queryKey, (old) => {
+      const newData = [...old];
+      newData[idx] = color;
+      return newData;
+    });
+  }
+
+  function savePalette() {
+    // Store the current data for comparison in the useEffect
+    const oldDataForScheduledActions = client.getQueryData(queryKey);
+    addToStack(oldDataForScheduledActions);
+    perfromAction();
+  }
+
+  function perfromAction() {
+    console.log({ isScheduled: isScheduled() });
+    if (isScheduled()) return;
+    console.log("saving");
+    startScheduling();
+    let timeoutId;
+    timeoutId = setTimeout(() => {
+      const newData = getLatestColorChanged();
+      mutate(newData, {
+        onSuccess: () => {
+          console.log({
+            newData,
+          });
+        },
+        onSettled: () => {
+          noMoreScheduling();
+        },
+      });
+      clearTimeout(timeoutId);
+    }, 4000);
+  }
+
+  function manualSave(){
+    const data = client.getQueryData()
+    mutate()
+  }
+  return { changePalette, savePalette ,savingState};
+}
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseClient = createClient("https://whgsfajxsfkedwplrtkf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoZ3NmYWp4c2ZrZWR3cGxydGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM1MjU3NDQsImV4cCI6MjAxOTEwMTc0NH0.NLzUW7kO0PJZbXBHb9lAqxB0_jHos--_bf6VWSBKdb4");
+import { SUPABASE_PROJECT_ID,SUPABASE_ANON_KEY } from "./data/data";
+const supabaseClient = createClient(SUPABASE_PROJECT_ID, SUPABASE_ANON_KEY);
 export default supabaseClient
