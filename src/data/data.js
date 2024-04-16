fc4aaae0d5222aac1e05dbe161e560af240a313b
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-04-16 22:43:56 +0530

    setup-- changed env

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "PickPalette"
-export const PRODUCTION_ENV = import.meta.env.ENV === "PRODUCTION"
+export const PRODUCTION_ENV = import.meta.env.VITE_ENV === "PRODUCTION"
 export const FOOTER_LINK = "https://www.linkedin.com/in/arnab-chatterjee-905ab3219/"
 export const ABOUT_LINK = "https://github.com/ArnabChatterjee20k"
 export const PALETTE_API = import.meta.env.VITE_PALETTE_API
